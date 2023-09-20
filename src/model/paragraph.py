141ddd6c5f34f14d97372a66b5c50223da50010d
adrien.aribaut-gaudin <adrien.aribaut-gaudin@epita.fr>
2023-09-20 16:51:04 +0200

    add: reader that can detect font-size

---
+++ src/model/paragraph.py
@@ -0,0 +1,5 @@
+class Paragraph:
+    def __init__(self, text : str, font_type : str):
+        self.text = text
+        self.font_type = font_type
+
---
+++ src/tools/reader.py
@@ -0,0 +1,48 @@
+import os
+import pdfplumber as pdfp
+import sys
+from src.model.paragraph import Paragraph
+
+def get_pdf_title_styles(path):
+    page_number = 1
+    with open(path, 'rb') as f:
+        reader = pdfp.PDF(f)
+        for page in reader.pages:
+            dictionary = page.extract_text_lines()# Lines maybe
+            for i in range(len(dictionary)):
+                p = Paragraph(dictionary[i]["text"],"unknown")
+                if(dictionary[i+1]):
+                    while(dictionary[i+1]["chars"][0]["size"] == dictionary[i]["chars"][0]["size"]):
+                        p.text += " " + dictionary[i+1]["text"]
+                        if dictionary[i]["chars"][0]["size"] >= 9 and dictionary[i]["chars"][0]["size"] < 12.8:
+                            p.font_type = "content"
+                        elif dictionary[i]["chars"][0]["size"] >= 12.8 and dictionary[i]["chars"][0]["size"] <= 13.5:
+                            p.font_type = "title4"
+                        elif dictionary[i]["chars"][0]["size"] > 13.5 and dictionary[i]["chars"][0]["size"] <= 15.5:
+                            p.font_type = "title3"
+                        elif dictionary[i]["chars"][0]["size"] > 15.5 and dictionary[i]["chars"][0]["size"] <= 18.5:
+                            p.font_type = "title2"
+                        elif dictionary[i]["chars"][0]["size"] > 19 and dictionary[i]["chars"][0]["size"] < 30:
+                            p.font_type = "title1"
+                        i += 1
+                        if(i == len(dictionary)-1):
+                            break
+                print(f'{i} : {p.font_type} ->>>>> {p.text}')
+            page_number += 1
+
+
+def test_get_font_sizes_of_a_page(page : int):
+    with open(os.path.abspath("../../../../../Downloads/Illumio_Core_REST_API_Developer_Guide_23.3.pdf"), 'rb') as f:
+        reader = pdfp.PDF(f)
+        page = reader.pages[page]
+        dictionary = page.extract_text_lines()
+        for i in range(len(dictionary)):
+            print(f'{i} : {dictionary[i]["chars"][0]["size"]} ->>>>> {dictionary[i]["text"]}')
+
+
+
+
+get_pdf_title_styles(os.path.abspath("../../../../../Downloads/Illumio_Core_REST_API_Developer_Guide_23.3.pdf"))
+# print("--------------------------------------------------")
+# print("--------------------------------------------------")
+#print(test_get_font_sizes_of_a_page(8))

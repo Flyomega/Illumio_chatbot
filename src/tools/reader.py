24b976382c2e36943afe56e92bb39015353b8288
adrien.aribaut-gaudin <adrien.aribaut-gaudin@epita.fr>
2023-09-20 17:45:55 +0200

    text detection

--- src/tools/reader.py
+++ src/tools/reader.py
@@ -1,7 +1,6 @@
 import os
 import pdfplumber as pdfp
-import sys
-from src.model.paragraph import Paragraph
+from paragraph import Paragraph
 
 def get_pdf_title_styles(path):
     page_number = 1
@@ -11,19 +10,19 @@ def get_pdf_title_styles(path):
             dictionary = page.extract_text_lines()# Lines maybe
             for i in range(len(dictionary)):
                 p = Paragraph(dictionary[i]["text"],"unknown")
-                if(dictionary[i+1]):
+                if dictionary[i]["chars"][0]["size"] >= 9 and dictionary[i]["chars"][0]["size"] < 12.8:
+                    p.font_type = "content"
+                elif dictionary[i]["chars"][0]["size"] >= 12.8 and dictionary[i]["chars"][0]["size"] <= 13.5:
+                    p.font_type = "title4"
+                elif dictionary[i]["chars"][0]["size"] > 13.5 and dictionary[i]["chars"][0]["size"] <= 15.5:
+                    p.font_type = "title3"
+                elif dictionary[i]["chars"][0]["size"] > 15.5 and dictionary[i]["chars"][0]["size"] <= 18.5:
+                    p.font_type = "title2"
+                elif dictionary[i]["chars"][0]["size"] > 19 and dictionary[i]["chars"][0]["size"] < 30:
+                    p.font_type = "title1"
+                if(i != len(dictionary)-1):
                     while(dictionary[i+1]["chars"][0]["size"] == dictionary[i]["chars"][0]["size"]):
                         p.text += " " + dictionary[i+1]["text"]
-                        if dictionary[i]["chars"][0]["size"] >= 9 and dictionary[i]["chars"][0]["size"] < 12.8:
-                            p.font_type = "content"
-                        elif dictionary[i]["chars"][0]["size"] >= 12.8 and dictionary[i]["chars"][0]["size"] <= 13.5:
-                            p.font_type = "title4"
-                        elif dictionary[i]["chars"][0]["size"] > 13.5 and dictionary[i]["chars"][0]["size"] <= 15.5:
-                            p.font_type = "title3"
-                        elif dictionary[i]["chars"][0]["size"] > 15.5 and dictionary[i]["chars"][0]["size"] <= 18.5:
-                            p.font_type = "title2"
-                        elif dictionary[i]["chars"][0]["size"] > 19 and dictionary[i]["chars"][0]["size"] < 30:
-                            p.font_type = "title1"
                         i += 1
                         if(i == len(dictionary)-1):
                             break
@@ -40,9 +39,7 @@ def test_get_font_sizes_of_a_page(page : int):
             print(f'{i} : {dictionary[i]["chars"][0]["size"]} ->>>>> {dictionary[i]["text"]}')
 
 
-
-
-get_pdf_title_styles(os.path.abspath("../../../../../Downloads/Illumio_Core_REST_API_Developer_Guide_23.3.pdf"))
+get_pdf_title_styles(os.path.abspath("../../data/Illumio_Core_REST_API_Developer_Guide_23.3.pdf"))
 # print("--------------------------------------------------")
 # print("--------------------------------------------------")
 #print(test_get_font_sizes_of_a_page(8))
